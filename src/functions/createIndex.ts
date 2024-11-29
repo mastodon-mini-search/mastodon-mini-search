f7a4f2fe462c9898492e41ce0cc47c12a6245a6f
merely <git@merely.ca>
2024-11-29 16:39:15 -0400

    use bigram

--- src/functions/createIndex.ts
+++ src/functions/createIndex.ts
@@ -1,6 +1,8 @@
 import MiniSearch from "minisearch"
 import { StatusStore } from "../models/StatusStore"
 import stripHTML from "./stripHTML"
+import { bigram } from "n-gram"
+import isCJKWord from "./isCJKWord"
 
 const SPACE_OR_PUNCTUATION = /[\n\r\p{Z}\p{P}]+/u
 
@@ -10,21 +12,42 @@ export default function(store: StatusStore) {
     idField: 'uri',
     tokenize(text) {
       // @ts-ignore
-      const segmenter = Intl.Segmenter && new Intl.Segmenter("zh", { granularity: "word" })
+      const wordSegmenter = new Intl.Segmenter(undefined, { granularity: 'word' })
+
       const tokens: string[] = []
-      // @ts-ignore
-      Array.from(segmenter.segment(text)).forEach(segment => {
+
+      const segments = text.split(SPACE_OR_PUNCTUATION)
+
+      segments.forEach(segment => {
         // @ts-ignore
-        const token: string = segment.segment.toLowerCase()
-        if (!token.match(SPACE_OR_PUNCTUATION)) {
-          tokens.push(token)
-        }
+        const words: string[] = Array.from(wordSegmenter.segment(segment)).map(s => s.segment)
+        const CJKSegments: string[] = ['']
+        words.forEach(word => {
+          if (isCJKWord(word)) {
+            CJKSegments[CJKSegments.length - 1] += word
+          } else {
+            CJKSegments.push('')
+            tokens.push(word.toLowerCase())
+          }
+        })
+        CJKSegments.forEach(segment => {
+          if (segment.length > 0) {
+            // @ts-ignore
+            bigram(segment).forEach(token => tokens.push(token))
+          }
+        })
       })
       return tokens
     },
     searchOptions: {
       combineWith: 'AND',
-      fuzzy: 0.35,
+      fuzzy(term) {
+        if (isCJKWord(term)) {
+          return false
+        } else {
+          return 0.35
+        }
+      },
       maxFuzzy: 4
     }
   })
