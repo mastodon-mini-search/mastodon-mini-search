ea687ee3510572d70846a4531e1f2596bf03bc0b
merely <git@merely.ca>
2024-11-29 22:11:37 -0400

    use pangu approach

--- src/functions/createIndex.ts
+++ src/functions/createIndex.ts
@@ -6,39 +6,31 @@ import isCJKWord from "./isCJKWord"
 import normalizeChinese from "./normalizeChinese"
 
 const SPACE_OR_PUNCTUATION = /[\n\r\p{Z}\p{P}]+/u
+const CJK_RANGE = '\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30fa\u30fc-\u30ff\u3100-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff'
+const CJK_NCJK = new RegExp(`([${CJK_RANGE}])([^${CJK_RANGE}])`, 'g')
+const NCJK_CJK = new RegExp(`([^${CJK_RANGE}])([${CJK_RANGE}])`, 'g')
+
+function addSpaceBetweenCJKandNonCJK(text: string) {
+  return text.replace(CJK_NCJK, '$1 $2').replace(NCJK_CJK, '$1 $2')
+}
 
 export default function(store: StatusStore) {
   const miniSearch = new MiniSearch({
     fields: ['content'],
     idField: 'uri',
     tokenize(text) {
-      // @ts-ignore
-      const wordSegmenter = new Intl.Segmenter(undefined, { granularity: 'word' })
-
       const tokens: string[] = []
 
-      const segments = text.split(SPACE_OR_PUNCTUATION)
+      const segments = addSpaceBetweenCJKandNonCJK(text).split(SPACE_OR_PUNCTUATION)
 
       segments.forEach(segment => {
-        // @ts-ignore
-        const words: string[] = Array.from(wordSegmenter.segment(segment)).map(s => s.segment)
-        const CJKSegments: string[] = ['']
-        words.forEach(word => {
-          if (isCJKWord(word)) {
-            CJKSegments[CJKSegments.length - 1] += word
-          } else {
-            CJKSegments.push('')
-            tokens.push(word)
-          }
-        })
-        CJKSegments.forEach(segment => {
-          if (segment.length > 0) {
-            const normalized = Array.from(segment).map(char => normalizeChinese(char))
-            normalized.forEach(char => tokens.push(char))
-            // @ts-ignore
-            bigram(normalized).forEach(tokenArray => tokens.push(tokenArray.join('')))
-          }
-        })
+        if (isCJKWord(segment)) {
+          const normalized = Array.from(segment).map(char => normalizeChinese(char))
+          normalized.forEach(char => tokens.push(char))
+          bigram(normalized).forEach(tokenArray => tokens.push(tokenArray.join('')))
+        } else {
+          tokens.push(segment)
+        }
       })
       return tokens
     },
